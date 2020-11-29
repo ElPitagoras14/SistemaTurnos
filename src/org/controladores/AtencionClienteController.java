4f000ef006ae48ccf31291f0eb84a20ad42fa2b5
Geovanny Rojas <geovanny_rl14@hotmail.com>
2020-11-28 19:02:58 -0500

    Muestro nombre del Paciente al ser atendido

--- src/org/controladores/AtencionClienteController.java
+++ src/org/controladores/AtencionClienteController.java
@@ -10,6 +10,7 @@ import MediaPlayer.VideoPlayer;
 import System.SistemaEspera;
 import java.io.IOException;
 import java.net.URL;
+import java.util.LinkedList;
 import java.util.Optional;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
@@ -22,6 +23,9 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.TextArea;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.text.Text;
+import org.unique.Paciente;
+import org.unique.Turno;
 
 /**
  * FXML Controller class
@@ -39,6 +43,9 @@ public class AtencionClienteController implements Initializable {
     @FXML
     private Button btnAtencion;
     
+    @FXML
+    private Text infoPaciente;
+    
     private SistemaEspera sistema;
 
     /**
@@ -49,6 +56,9 @@ public class AtencionClienteController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         sistema = SistemaEspera.getInstance();
+        LinkedList<Turno> pacientes = (LinkedList<Turno>) sistema.getListaTurno();
+        Paciente p = pacientes.getFirst().getPaciente();
+        infoPaciente.setText("Paciente:"+p.getNombre()+" "+p.getApellido()+" de "+p.getEdad()+" años.\nSintoma:"+p.getSintoma().toString());
     }
     
     @FXML
