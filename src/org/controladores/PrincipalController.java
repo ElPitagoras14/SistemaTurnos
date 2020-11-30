741fc6b7319a009ba242532261459a7198172a93
ElPitagoras14 <jonfrgar@espol.edu.ec>
2020-11-29 21:02:17 -0500

    Code Smell

--- src/org/controladores/PrincipalController.java
+++ src/org/controladores/PrincipalController.java
@@ -28,13 +28,8 @@ import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.CornerRadii;
-import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
-import javafx.scene.paint.Paint;
-import javafx.scene.text.Font;
-import javafx.scene.text.Text;
-import javafx.scene.text.TextAlignment;
 
 /**
  *
